0436542d97837cf9843b876c3f1f522a3635bc4c
UbiraEFC <ubirata.efc@gmail.com>
2022-10-10 16:53:37 -0300

    Created S3 Storage

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,6 +4,7 @@ import { DayjsDateProvider } from "./DateProvider/implementations/DayjsDateProvi
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
 import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProviser";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 
@@ -17,8 +18,13 @@ container.registerInstance<IMailProvider>(
 	new EtherealMailProvider(),
 );
 
+const diskStorage = {
+	local: LocalStorageProvider,
+	s3: S3StorageProvider,
+}
+
 container.registerSingleton<IStorageProvider>(
 	"StorageProvider",
-	LocalStorageProvider
+	diskStorage[process.env.disk],
 )
 
