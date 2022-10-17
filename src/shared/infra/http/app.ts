e32802ca73f516f3ecde0ad54abb6dc223a3edc7
UbiraEFC <ubirata.efc@gmail.com>
2022-10-17 16:42:34 -0300

    Sentry

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,8 +4,8 @@ import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
-// import * as Sentry from "@sentry/node";
-// import * as Tracing from "@sentry/tracing";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import { router } from "./routes";
 import swaggerFile from "../../../swagger.json";
@@ -22,17 +22,17 @@ const app = express();
 
 app.use(rateLimiter);
 
-// Sentry.init({
-// 	dsn: process.env.SENTRY_DSN,
-// 	integrations: [
-// 		new Sentry.Integrations.Http({ tracing: true }),
-// 		new Tracing.Integrations.Express({ app }),
-// 	],
-// 	tracesSampleRate: 1.0,
-// });
+Sentry.init({
+	dsn: process.env.SENTRY_DSN,
+	integrations: [
+		new Sentry.Integrations.Http({ tracing: true }),
+		new Tracing.Integrations.Express({ app }),
+	],
+	tracesSampleRate: 1.0,
+});
 
-// app.use(Sentry.Handlers.requestHandler());
-// app.use(Sentry.Handlers.tracingHandler());
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
 
@@ -43,7 +43,7 @@ app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
 app.use(router);
-// app.use(Sentry.Handlers.errorHandler());
+app.use(Sentry.Handlers.errorHandler());
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
 	if (err instanceof AppError) {
